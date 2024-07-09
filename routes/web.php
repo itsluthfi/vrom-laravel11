d93a71576a1ddc79c3592434dc127cf3bc03de7c
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2024-07-09 20:33:55 +0700

    feat: set route for admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,16 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
+Route::prefix('admin')->name('admin.')->middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
 });
