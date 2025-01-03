2350cf890f2d526f41023bc60ede4071c144e633
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2025-01-03 22:25:19 +0700

    feat: create brand pages

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link href="{{ route('admin.dashboard') }}" :active="request()->routeIs('admin.dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('admin.brands.index') }}" :active="request()->routeIs('admin.brands.index')">
+                        {{ __('Brand') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -152,6 +155,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link href="{{ route('admin.dashboard') }}" :active="request()->routeIs('admin.dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{ route('admin.brands.index') }}" :active="request()->routeIs('admin.brands.index')">
+                {{ __('Brand') }}
             </x-responsive-nav-link>
         </div>
 
