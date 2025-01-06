cbce444d6ab0b2f24d85283c591485929c235074
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2025-01-06 21:09:55 +0700

    feat: add type controller to web route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 use App\Http\Controllers\Admin\BrandController as AdminBrandController;
+use App\Http\Controllers\Admin\TypeController as AdminTypeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,4 +17,5 @@ Route::prefix('admin')->name('admin.')->middleware([
 ])->group(function () {
     Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
     Route::resource('brands', AdminBrandController::class);
+    Route::resource('types', AdminTypeController::class);
 });
