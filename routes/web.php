7285cfba54ccf63e8fab3851d3e62cddcf2ade2f
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2025-01-07 22:23:42 +0700

    feat: create item request and controller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 use App\Http\Controllers\Admin\BrandController as AdminBrandController;
 use App\Http\Controllers\Admin\TypeController as AdminTypeController;
+use App\Http\Controllers\Admin\ItemController as AdminItemController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,4 +19,5 @@ Route::prefix('admin')->name('admin.')->middleware([
     Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
     Route::resource('brands', AdminBrandController::class);
     Route::resource('types', AdminTypeController::class);
+    Route::resource('items', AdminItemController::class);
 });
