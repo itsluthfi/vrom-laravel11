0f6d689c27d98ffe279b3a56498cf69e94bad35c
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2025-01-13 13:36:50 +0700

    feat: create booking pages

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -24,6 +24,9 @@
                     <x-nav-link href="{{ route('admin.items.index') }}" :active="request()->routeIs('admin.items.index')">
                         {{ __('Item') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('admin.bookings.index') }}" :active="request()->routeIs('admin.bookings.index')">
+                        {{ __('Booking') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -170,6 +173,9 @@
             </x-responsive-nav-link>
             <x-responsive-nav-link href="{{ route('admin.items.index') }}" :active="request()->routeIs('admin.items.index')">
                 {{ __('Item') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{ route('admin.bookings.index') }}" :active="request()->routeIs('admin.bookings.index')">
+                {{ __('Booking') }}
             </x-responsive-nav-link>
         </div>
 
